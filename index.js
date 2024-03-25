3e9a0374c79679727d9325cbfbdeb132a3e3288d
Daniel Rodrigues <daniel.r.silva@globalhitss.com.br>
2024-03-25 15:45:28 -0300

    suavizando a animação

--- index.js
+++ index.js
@@ -101,7 +101,25 @@ function draw() {
   score.draw();
   ball.draw();
 }
-setup();
-draw();
 
-window.setInterval(draw, 1000 / 50)
+window.animationFrame = (function () {
+  return (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+      return window.setTimeout(callback, 100 / 60)
+    }
+
+  )
+})()
+
+function main() {
+  animationFrame(main)
+  draw()
+}
+
+setup();
+main()
