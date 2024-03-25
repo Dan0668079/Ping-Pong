96f66335b5ed7677116a81d6b750e741ee2ec2d7
Daniel Rodrigues <daniel.r.silva@globalhitss.com.br>
2024-03-25 09:44:46 -0300

    feat index.js  desenhando o placar

--- index.js
+++ index.js
@@ -8,14 +8,12 @@ function setup() {
   canvasCtx.width = window.innerWidth;
   canvasEl.height = window.innerHeight;
   canvasCtx.height = window.innerHeight;
-
 }
+
 function draw() {
   //! Desenho do Campo
   canvasCtx.fillStyle = ('#286047')
   canvasCtx.fillRect(0, 0, window.innerWidth, window.innerHeight)
-
-
   //! Desenho Linha Central
   canvasCtx.fillStyle = '#ffffff';
 
@@ -25,13 +23,21 @@ function draw() {
   const h = window.innerHeight
 
   canvasCtx.fillRect(x, y, w, h)
-
   //! Desenho Raquete esquerda
   canvasCtx.fillRect(10, 100, lineWidth, 200)
   //! Desenho Raquete esquerda
   canvasCtx.fillRect(window.innerWidth - lineWidth - 10, 250, lineWidth, 200)
-
+  //! Desenho da bolinha
+  canvasCtx.beginPath()
+  canvasCtx.arc(200, 300, 20, 0, 2 * Math.PI, false)
+  canvasCtx.fill()
+  //! Desenho o placar
+  canvasCtx.font = "bold 72px Arial"
+  canvasCtx.textAlign = "center"
+  canvasCtx.textBaseline = "top"
+  canvasCtx.fillStyle = "#01341D"
+  canvasCtx.fillText("3", window.innerWidth * 0.25, 50)
+  canvasCtx.fillText("1", window.innerWidth * 0.75, 50)
 }
-
 setup();
 draw();
